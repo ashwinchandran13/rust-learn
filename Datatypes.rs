964cff8150e0b372950c23c767684fc047da8bb0
Ashwin Chandran <ashwinchandran13@gmail.com>
2022-06-07 23:27:30 +0530

    begins

---
+++ Datatypes.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let company_string = "neoito";
+    let rating_float = 4.5;
+    let is_growing_boolean = true;
+    let icon_char = '♥';
+    
+    println!("company name is: {}", company_string);
+    println!("company rating on 5 is: {}", rating_float);
+    println!("company is growing: {}", is_growing_boolean);
+    println!("company icon is: {}", icon_char);
+}
---
+++ Integer.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let result = 10; // default signed integer or i32
+    let age:u32 = 20;
+    let sum:i32 = 5-15;
+    let mark:isize = 10;
+    let count:usize = 30;
+    println!("result value is: {}", result);
+    println!("age value is: {}", age);
+    println!("sum value is: {}", sum);
+    println!("mark value is: {}", mark);
+    println!("count value is: {}", count);
+}
