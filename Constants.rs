38edfb87033f625af41bc8001cd975ba09df0024
Ashwin Chandran <ashwinchandran13@gmail.com>
2022-06-09 22:19:50 +0530

    variable, const and shadow

---
+++ Constants.rs
@@ -0,0 +1,7 @@
+fn main() {
+    const USER_LIMIT = 100; // const must have data type
+    const PI:f32 = 3.14;
+
+    println!("user limit is {}", USER_LIMIT);
+    println!("pi value is {}", PI);
+}
---
+++ ShadowVariables.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let salary = 100.50;
+    let salary = 1;   // shadow variable
+    println!("the value of salary is {}", salary);
+}
